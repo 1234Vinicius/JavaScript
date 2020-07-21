046a7c2607c6a14a31ae9ff3c7c73e3884db8e9c
1234Vinicius <99887799vinicius@gmail.com>
2020-07-20 21:41:48 -0300

    Adicionando um novo projeto

---
+++ Curso JS/aula001/ex001.js
@@ -0,0 +1,8 @@
+
+    <script>
+        window.alert("Minha primeira mensagem! ")
+        window.alert("Está gostando de JS?") // Janela com botão ok
+        window.alert("Qual é o seu nome?")  // Vai perguntar o que?
+    </script>
+
+    
