156fc4f94a19d5429493a6bb3acc8e46eadc0d0d
1234Vinicius <99887799vinicius@gmail.com>
2020-07-20 21:52:37 -0300

    Adicionando novos exemplos

--- Curso JS/aula001/ex004.js
+++ Curso JS/aula001/ex004.js
@@ -1,13 +1,1 @@
-
-  
-    
-    
-    <script>
-        var n1 = Number.parseInt (window.prompt('Digite um número: ')) // String
-        var n2 = Number.parseInt(window.prompt('Digite outro número: ')) // String
-        var s = n1 + n2
-        window.alert('A soma dos valores é ' + s) // Concatenação
-        // (number + number) para adição
-        //)String + String) para concatenação 
-        
-    </script>
+<script type="text/javascript">document.write(Date());</script>
